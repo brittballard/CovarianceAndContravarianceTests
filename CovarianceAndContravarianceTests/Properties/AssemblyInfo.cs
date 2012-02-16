d22e813104c7ec39d19034f58a5f300d197bbb73
Britt Ballard <davidbritton.ballard@gmail.com>
2012-02-16 11:17:22 -0600

    First commit.

---
+++ CovarianceAndContravarianceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CovarianceAndContravarianceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GameStop, Inc.")]
+[assembly: AssemblyProduct("CovarianceAndContravarianceTests")]
+[assembly: AssemblyCopyright("Copyright © GameStop, Inc. 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c8640995-7b8d-4fa0-b622-18bde535281c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CovarianceAndContravarianceTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CovarianceAndContravarianceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Func_Generics_Parameter_Contravariance()
+        {
+            var func = new Func<Person, Person>(p => p);
+            
+            // paramatere contravariant: from person -> employee (from "narrower" -> to "wider")
+            Person animal = func(new Employee { Age = 32 });
+            Assert.IsInstanceOfType(animal, typeof(Employee));
+        }
+
+        [TestMethod]
+        public void Func_Generics_ReturnType_Covariance()
+        {
+            var func1 = new Func<Person, Person>(p => { p.Name = "britton"; return p; });
+            var func2 = new Func<Person, Animal>(p => { p.Name = "cassie"; return p; });
+
+            func2 = func1;
+
+            // paramatere contravariant: from person -> employee (from "narrower" -> to "wider")
+            Animal returnValue = func2(new Employee { Age = 32 });
+            Assert.IsInstanceOfType(returnValue, typeof(Person));
+            Assert.AreEqual("britton", ((Person)returnValue).Name);
+        }
+    }
+
+    public class Animal
+    {
+        public int Age { get; set; }
+    }
+
+    public class Person : Animal
+    {
+        public string Name { get; set; }
+    }
+
+    public class Employee : Person
+    {
+    }
+}
